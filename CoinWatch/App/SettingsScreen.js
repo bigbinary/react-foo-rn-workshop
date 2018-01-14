dba9b5cfafba320c2e8916aa3c3b8c60aa0ad1f9
Chirag Shah <shahchirag1709@gmail.com>
2018-01-14 17:47:58 +0530

    Used AsyncStorage for saving user preference

--- CoinWatch/App/SettingsScreen.js
+++ CoinWatch/App/SettingsScreen.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, AsyncStorage } from 'react-native';
+const BASE_CURRENCY_KEY = 'coinwatcher:baseCurrency';
 
 export default class SettingsScreen extends Component {
+  state = {
+    baseCurrency: 'USD',
+  };
+
+  componentWillMount() {
+    AsyncStorage.getItem(BASE_CURRENCY_KEY).then(baseCurrency => {
+      this.setState({
+        baseCurrency: baseCurrency || 'USD',
+      });
+    });
+  }
+
   static navigationOptions = ({ navigation }) => ({
     title: 'Settings',
   });
 
+  changeCurrency(baseCurrency) {
+    this.setState({ baseCurrency });
+    AsyncStorage.setItem(BASE_CURRENCY_KEY, baseCurrency);
+  }
+
   render() {
     return (
-      <View>
-        <Text>Setting options...</Text>
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <View
+          style={{
+            margin: 10,
+            padding: 10,
+            borderBottomWidth: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Text>Base Currency</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Button
+              title="USD"
+              onPress={() => this.changeCurrency('USD')}
+              color={this.state.baseCurrency === 'USD' ? 'green' : 'grey'}
+            />
+            <Button
+              title="INR"
+              onPress={() => this.changeCurrency('INR')}
+              color={this.state.baseCurrency === 'INR' ? 'green' : 'grey'}
+            />
+          </View>
+        </View>
       </View>
     );
   }
